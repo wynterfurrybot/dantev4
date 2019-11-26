05dfa48465f986c6c22c4af38f7c7e0b01541848
Darkmane Arweinydd <mcpchalie@gmail.com>
2019-11-26 00:50:32 +0000

    Logging channels to be replaced with web API
    A web API will replace all log channels except from #case_logs - this is to be powered by PHP & MySQL

--- log.js
+++ log.js
@@ -7,57 +7,109 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "dante"
 });
 
-x.on("messageDelete", (messageDelete) => {
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 })
 
-x.on('messageUpdate', (oldMessage, newMessage) => {
-})
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
 
-x.on('channelCreate', channel => {
-})
+client.on('message', msg => {
+// Log to new messages table
 
-x.on('channelDelete', channel => {
+if(msg.attachments)
+{
+  var a = msg.attachments;
+  a.forEach(function (b)
+{
+  msg.content = msg.content + " -- " + b.url;
 })
+}
 
-x.on('channelDelete', channel => {
-})
+con.query("INSERT INTO messages (id, content, guild_id, author) VALUES(?, ?, ?, ?)", [msg.id, msg.content, msg.guild.id, msg.author.id], async function(err, result, fields) {
+console.log("1 record inserted");
+// send to local message channel
+});
+});
 
-x.on('guildMemberAdd', member => {
-})
 
-x.on('guildMemberRemove', member => {
-})
+client.on("messageDelete", (messageDelete) => {
+  // Log to logging Channel Only
 
-x.on('userUpdate', (oldmember, newmember) => {
-})
-
-x.on('messageReactionAdd', (reaction, usr) => {
-})
-
-x.on('messageReactionRemove', (reaction, usr) => {
-})
-
-x.on('guildMemberUpdate', (oldmember, newmember) => {
-})
-
-x.on('guildBanAdd', (guild, member) => {
-})
-
-x.on('guildBanRemove', (guild, member) => {
-})
-
-x.on("guildCreate", guild => {
+  if(msg.attachments)
+  {
+    var a = msg.attachments;
+    a.forEach(function (b)
+  {
+    msg.content = msg.content + " -- " + b.url;
   })
+  }
+});
 
-x.on("guildDelete", guild => {
-    })
-    
+client.on('messageUpdate', (oldMessage, newMessage) => {
+  if(newMessage.attachments)
+  {
+    var a = newMessage.attachments;
+    a.forEach(function (b)
+  {
+    newMessage.content = newMessage.content + " -- " + b.url;
+  })
+  }
+  con.query("INSERT INTO edithistory (id, oldcontent, newcontent) VALUES(?, ?, ?)", [oldMessage.id, oldMessage.content, newMessage.content], async function(err, result, fields) {
+  console.log("1 record inserted");
 
+  // send to local message channel
+  });
+});
 
+client.on('channelCreate', channel => {
+});
+
+client.on('channelDelete', channel => {
+});
+
+client.on('channelDelete', channel => {
+});
+
+client.on('guildMemberAdd', member => {
+});
+
+client.on('guildMemberRemove', member => {
+});
+
+client.on('userUpdate', (oldmember, newmember) => {
+});
+
+client.on('messageReactionAdd', (reaction, usr) => {
+});
+
+client.on('messageReactionRemove', (reaction, usr) => {
+});
+
+client.on('guildMemberUpdate', (oldmember, newmember) => {
+});
+
+client.on('guildBanAdd', (guild, member) => {
+});
+
+client.on('guildBanRemove', (guild, member) => {
+});
+
+client.on("guildCreate", guild => {
+});
+
+client.on("guildDelete", guild => {
 });
 
 client.login('MyToken');
