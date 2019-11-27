4f077e04dd0af81ee4863c7fa7bc3da4f3fb5619
Darkmane Arweinydd <mcpchalie@gmail.com>
2019-11-27 22:04:35 +0000

    Beta release

--- dante.py
+++ dante.py
@@ -10,12 +10,17 @@ class MyClient(discord.Client):
     async def on_message(self, message):
 
         prefix = "!"
+        message.content = message.content.lower()
         fun = __import__("fun")
         await fun.msg(str(message.content), message, prefix, self)
         info = __import__("info")
         await info.msg(str(message.content), message, prefix, self)
+        mod = __import__("mod")
+        await mod.msg(str(message.content), message, prefix, self)
         #nsfw = __import__("nsfw.py")
-        #roomservice = __import__("roomservice.py")
+        if not message.author.bot:
+            if message.content.startswith("!"):
+                await message.delete()
 
 client = MyClient()
-client.run('MyToken')
+client.run('MYTOKEN')
