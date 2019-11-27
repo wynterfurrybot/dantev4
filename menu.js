3896a239555ef01320150e17f08a5786d8c26b05
Darkmane Arweinydd <mcpchalie@gmail.com>
2019-11-27 22:09:45 +0000

    Beta release

---
+++ menu.js
@@ -0,0 +1,43 @@
+function get(msg, item, id, callback) {
+
+
+		if(item == "!beer" || item == "!whiskey" || item == "!vodka" || item == "!martini" || item === "rum"){
+
+			if(id === "347912401812848640")
+			{
+				if (msg.channel.id === "463840305129455616"){
+					callback("I shouldn't really serve this.. but here's a " + item.replace("!","") + "\nEnjoy!");
+					return;
+				}
+				else{
+				callback("Darkmane told me not to serve you anything alcoholic.. ");
+				return;
+				}
+			}
+
+	      callback("thanks for ordering a " + item.replace("!","") + "! \n\nDrink responsibly!")
+			}
+
+			else if(item == "!cola" || item == "!tea" || item == "!coffee" || item == "!coke" )
+				{
+					 callback("thanks for ordering a " + item.replace("!","") + "! \n\nWe respect your decision to not get wasted!")
+				}
+
+
+				else if (item === "!pineapple" || item === "!pineapples"){
+					callback(":pineapple:");
+				}
+
+
+
+
+
+
+	else{
+		callback("Not a drink");
+	}
+}
+
+module.exports = {
+  get: get
+}
