8703681bb42078cb2a2d8b778d5682f870172df2
Darkmane Arweinydd <mcpchalie@gmail.com>
2019-11-27 22:03:33 +0000

    Beta release

--- fun.py
+++ fun.py
@@ -18,12 +18,32 @@ async def msg(message, x, p, self):
         for member in x.mentions:
             print(member)
             embed = discord.Embed(title = "Hug!", description = "<@" + str(msg.author.id) + "> has given <@" + str(member.id) + "> a hug!" , color=0x00ff00)
-            embed.set_image(url = "https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg")
+            if member.id == msg.author.id:
+                embed.description = "<@" + str(msg.author.id) + "> has hugged themselves! \n\nWhat a loner.."
+            embed.set_thumbnail(url = "https://pm1.narvii.com/6362/398e5e2edeed52fc23d9e85cbbbbe6e5b3951635_hq.jpg")
+            await msg.channel.send(embed = embed)
+
+    if message.startswith(p + "glomp"):
+        for member in x.mentions:
+            print(member)
+            embed = discord.Embed(title = "Glomp!", description = "<@" + str(msg.author.id) + "> has flopped on top of <@" + str(member.id) + ">!" , color=0x00ff00)
+            if member.id == msg.author.id:
+                embed.description = "<@" + str(msg.author.id) + "> has glomped themselves! \n\nWhat a loner.."
+            embed.set_thumbnail(url = "https://d.facdn.net/art/zaezar/1502813432/1502813432.zaezar_glomp.png")
             await msg.channel.send(embed = embed)
 
     if message.startswith(p + "cookie"):
         for member in x.mentions:
             print(member)
             embed = discord.Embed(title = "Cooooooooookieesss!", description = "<@" + str(msg.author.id) + "> has given <@" + str(member.id) + "> a cookie!" , color=0x00ff00)
-            embed.set_image(url = "https://cdn.shopify.com/s/files/1/0043/8471/8938/products/154360576926714891_812x.jpg?v=1555127662")
+            embed.set_thumbnail(url = "https://cdn.shopify.com/s/files/1/0043/8471/8938/products/154360576926714891_812x.jpg?v=1555127662")
+            await msg.channel.send(embed = embed)
+
+    if message.startswith(p + "lick"):
+        for member in x.mentions:
+            print(member)
+            embed = discord.Embed(title = "Slurp!", description = "<@" + str(msg.author.id) + "> has licked <@" + str(member.id) + ">, giving them a bath!" , color=0x00ff00)
+            if member.id == msg.author.id:
+                embed.description = "<@" + str(msg.author.id) + "> has gieven themselves a bath!"
+            embed.set_thumbnail(url = "https://t2.ea.ltmcdn.com/en/images/0/7/5/why_do_cats_lick_each_other_2570_600_square.jpg")
             await msg.channel.send(embed = embed)
