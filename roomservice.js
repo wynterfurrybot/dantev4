62c2d5a969239adb439e649feee3ca2835b6f322
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-08-10 21:29:16 +0100

    Added new welcome messages & onjoin events

--- roomservice.js
+++ roomservice.js
@@ -9,17 +9,153 @@ console.log("Warning".yellow);
 console.log("Log".cyan);
 console.log("Success".green);
 
-const Discord = require('discord.js');
-const fc = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+const fc = new Client();
+
+var invites = {};
 
 fc.on('ready', () => {
     console.log(`Logged in as ${fc.user.tag}!`.green);
     fc.user.setActivity(`Eating Pi | !help`);
+
+    fc.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+})
+})
 });
 
 var regex = RegExp(/[^a-zA-Z\d\s:]/g);
 
+fc.on('guildMemberRemove', member => {
+  if (member.guild.id === "667466143585402900"){
+
+    fc.channels.fetch("667495189832794162").then(channel =>{
+      channel.send(member.user.username + " has left, they will be missed")
+    })
+  }
+})
+
 fc.on('guildMemberAdd', member => {
+  if(member.guild.id === "736969969404870688"){
+    	member.send("Welcome to the elite server of european furries! \n\nJust so you know, we operate a scrict no robot policy here! \n\nTo verify you're not one, please go to https://verify.furrycentr.al/ef/ and log in with discord.");
+  }
+  if (member.guild.id === "667466143585402900"){
+
+    fc.channels.fetch("667495189832794162").then(channel =>{
+      channel.send("<@" + member.id + "> has joined the server!")
+    })
+
+    member.send("Hello " + member.user.username + " and welcome to Cuddle Club! Please answer these small questions and post them in the initiation (<#667495506251087902>) channel within the server! \n\nName:\nAge:\nPronouns:\nTimezone:\nA little bit about yourself:\nHow you found the server:\n\nOnce you fill this out please give staff 24 hours to review and approve you")
+
+  }
+
+  if (member.guild.id === "462041783438934036" || member.guild.id === "667466143585402900" || member.guild.id === "736969969404870688"){
+    member.guild.fetchInvites().then(guildInvites => {
+     try{
+       const ei = invites[member.guild.id];
+
+      invites[member.guild.id] = guildInvites;
+
+       const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+
+       const inviter = fc.users.get(invite.inviter.id);
+
+       if(member.guild.id === "736969969404870688"){
+         fc.channels.fetch("736981136164782171").then(channel =>{
+
+         const embed = new MessageEmbed()
+        // Set the title of the field
+        .setTitle('New Joiner')
+        // Set the color of the embed
+        .setColor(0xff0000)
+        // Set the main content of the embed
+        .setDescription(member.user.username + ' Joined using invite code ' + invite.code + ' made by <@' + invite.inviter.id + '> (' + inviter.username + ')');
+       channel.send(embed);
+     })
+   }
+
+       else if (member.guild.id === "667466143585402900"){
+         fc.channels.fetch("713624679671136306").then(channel =>{
+
+         const embed = new MessageEmbed()
+        // Set the title of the field
+        .setTitle('New Joiner')
+        // Set the color of the embed
+        .setColor(0xff0000)
+        // Set the main content of the embed
+        .setDescription(member.user.username + ' Joined using invite code ' + invite.code + ' made by <@' + invite.inviter.id + '> (' + inviter.username + ')');
+       channel.send(embed);
+       })
+       }
+
+       else if (member.guild.id === "462041783438934036"){
+            fc.channels.fetch("539917043886063636").then(channel =>{
+              const embed = new MessageEmbed()
+             // Set the title of the field
+             .setTitle('New Joiner')
+             // Set the color of the embed
+             .setColor(0xff0000)
+             // Set the main content of the embed
+             .setDescription(member.user.username + ' Joined using invite code ' + invite.code + ' made by <@' + invite.inviter.id + '> (' + inviter.username + ')');
+          channel.send(embed);
+
+          })
+        }
+     }
+
+     catch{
+       if (member.guild.id === "667466143585402900"){
+         fc.channels.fetch("713624679671136306").then(channel =>{
+
+         const embed = new MessageEmbed()
+        // Set the title of the field
+        .setTitle('New Joiner')
+        // Set the color of the embed
+        .setColor(0xff0000)
+        // Set the main content of the embed
+        .setDescription("I can't quite figure out how " + member.user.username + " joined the server. \n\nMaybe they used a temporary invite?")
+       channel.send(embed);
+
+
+       })
+       }
+
+       else if (member.guild.id === "736969969404870688"){
+         fc.channels.fetch("736981136164782171").then(channel =>{
+
+         const embed = new MessageEmbed()
+        // Set the title of the field
+        .setTitle('New Joiner')
+        // Set the color of the embed
+        .setColor(0xff0000)
+        // Set the main content of the embed
+        .setDescription("I can't quite figure out how " + member.user.username + " joined the server. \n\nMaybe they used a temporary invite?")
+       channel.send(embed);
+
+
+       })
+       }
+
+       else if (member.guild.id === "462041783438934036"){
+            fc.channels.fetch("539917043886063636").then(channel =>{
+              const embed = new MessageEmbed()
+             // Set the title of the field
+             .setTitle('New Joiner')
+             // Set the color of the embed
+             .setColor(0xff0000)
+             // Set the main content of the embed
+             .setDescription("I can't quite figure out how " + member.user.username + " joined the server. \n\nMaybe they used a temporary invite?")
+          channel.send(embed);
+
+          })
+        }
+     }
+
+ })
+}
+
+
 
 	if(regex.test(member.user.username.match))
 	{
@@ -58,7 +194,7 @@ fc.on('guildMemberAdd', member => {
 	}
 
   if(member.guild.id != "462041783438934036") return;
-	member.send("Welcome to The Floof Hotel! \n\nJust so you know, we operate a scrict no robot policy here! \n\nTo verify you're not one, please go to https://verify.furrycentr.al/?id=" + member.id);
+	member.send("Welcome to The Floof Hotel! \n\nJust so you know, we operate a scrict no robot policy here! \n\nTo verify you're not one, please go to https://verify.furrycentr.al/ and log in with discord.");
 })
 
 fc.on('guildMemberUpdate', (oldmem, newmem) => {
@@ -140,7 +276,7 @@ if(regex.test(newmem.user.username) && oldmem.user.username != newmem.user.usern
 	var hasmember = false;
 
 	oldroles.forEach(function(role){
-		if (role.id === "462042250612965386" || role.id === '634062572961923083')
+		if (role.id === "462042250612965386" || role.id === '725462565852938301' || role.id === '667472170926080011' || role.id === "736971909362614362")
 		{
 			hasmember = true;
 		}
@@ -148,18 +284,30 @@ if(regex.test(newmem.user.username) && oldmem.user.username != newmem.user.usern
 	})
 
 	roles.forEach(function(role){
+
+    if (role.id === "736971909362614362" && hasmember === false)
+    {
+      fc.channels.fetch("736979363362373642").then(channel => channel.send("<a:wel:742119488366837780><a:come:742119500068946084>"));
+      fc.channels.fetch("736979363362373642").then(channel => channel.send("Welcome <@" + newmem.id + "> to the most elite group of european furs! \n\nI hope you enjoy your stay here, and here's a free cookie to welcome you! :cookie:"));
+    }
+
+    if (role.id === "725462565852938301" && hasmember === false)
+		{
+      fc.channels.fetch("725476791858495508").then(channel => channel.send("<a:wel:742119488366837780><a:come:742119500068946084>"));
+			fc.channels.fetch("725476791858495508").then(channel => channel.send("Welcome <@" + newmem.id + "> to the coolest club around! Club Floof! \n\nFeel free to go ahead and grab some roles in <#742103877582848173>!\n\nI hope you enjoy your stay here, and here's a free cookie to welcome you! :cookie: \n\n<@&736666911189893170> Please welcome the above user!"));
+
+    }
+
 		if (role.id === "462042250612965386" && hasmember === false)
 		{
-			fc.channels.fetch("629056060803645453").then(channel => channel.send("<@" + newmem.id + ">, Welcome, welcome! We hope you enjoy your stay at The Floof Hotel!  *hands room card* \n\nYour room number is " + Math.floor(Math.random() * 5670) + "\n\nOn the desk is a free :cookie:, should you need us at any point, feel free to ping a member of staff! Should you have any feedback about anything, feel free to visit <#681616342733815825>! \n\nFeel free to tell us about you and your fursona in <#552957610899275776>"));
-
-			fc.channels.fetch("629056060803645453").then(channel => channel.send("<@&463088144292511764> Please welcome the above user!"));
-
+      fc.channels.fetch("629056060803645453").then(channel => channel.send("<a:wel:742119488366837780><a:come:742119500068946084>"));
+      fc.channels.fetch("629056060803645453").then(channel => channel.send("<@" + newmem.id + ">, Welcome, welcome! We hope you enjoy your stay at The Floof Hotel!  *hands room card* \n\nYour room number is " + Math.floor(Math.random() * 5670) + "\n\nOn the desk is a free :cookie:, should you need us at any point, feel free to ping a member of staff! Should you have any feedback about anything, feel free to visit <#681616342733815825>! \n\nFeel free to tell us about you and your fursona in <#552957610899275776> \n\n<@&463088144292511764> Please welcome the above user!"));
 		}
 
-    if(role.id === '634062572961923083' && hasmember === false)
+    if(role.id === '667472170926080011' && hasmember === false)
     {
-      	fc.channels.fetch("634065484052168715").then(channel => channel.send("Welcome <@" + newmem.id + "> to the resort! \n\nTo welcome you, we'd like to offer you a free :cookie:! \n\nWe hope you enjoy your stay here. \n\nWant to introduce yourself? <#634065279596363777> \nReady to chat? <#634060865750040588> \nWant to see the channels we offer? <#634080278222209034> \nGet some roles! > <#634070789326372884> / <#634071049905897505>"));
-        fc.channels.fetch("634065484052168715").then(channel => channel.send("<@&635161004539641867> Please welcome the above user!"));
+        fc.channels.fetch("667466143585402903").then(channel => channel.send("<a:wel:742119488366837780><a:come:742119500068946084>"));
+        fc.channels.fetch("667466143585402903").then(channel => channel.send("Welcome <@" + newmem.id + "> to cuddle club! \n\nPlease accept a free cookie, on me! :cookie:"));
 
     }
 
@@ -170,11 +318,15 @@ if(regex.test(newmem.user.username) && oldmem.user.username != newmem.user.usern
 
 fc.on('messageUpdate', (msg, newmsg) => {
 
-if(msg.channel.id === "717430439396245577")
+  if (newmsg.content.includes("dark") && newmsg.content.includes('cute')){
+    newmsg.delete()
+  }
+
+if(msg.channel.id === "717430439396245577" || msg.channel.id === "721808001098448896" || msg.channel.id === "717858360430428160")
 	{
 
 		// On receive message, (Given string `msg`) from channel #awoo
-if (!/^(\*|_)*awo+f?(!|\*|_)*( ?(:3|<3|owo|uwu))?( ?â¤ï¸)?(\*|_)*$/ui.test(newmsg.content)) {
+if (!/^(\*|_)*awo+f?(!|\*|_)*( ?(:3|<3|owo|uwu))?( ?❤️)?(\*|_)*$/ui.test(newmsg.content)) {
 newmsg.delete();
 newmsg.author.send("I see you.. No " + msg.content + ", only awoo!");
 }
@@ -182,7 +334,6 @@ newmsg.author.send("I see you.. No " + msg.content + ", only awoo!");
 })
 
 fc.on('message', msg => {
-
 
 	if(msg.channel.id === "462044347794456605" && msg.author.id === "155149108183695360")
 	{
@@ -202,11 +353,12 @@ fc.on('message', msg => {
       msg.channel.send("<@" + msg.author.id + ">, I've reposted your advertisement to <#629073904882810910> - it shall be displayed for 3 days. \n\n<@&462043912169586699>")
     }
 
-	if(msg.channel.id === "717430439396245577")
+	if(msg.channel.id === "717430439396245577" || msg.channel.id === "721808001098448896" || msg.channel.id === "717858360430428160")
+
 	{
 
 		// On receive message, (Given string `msg`) from channel #awoo
-if (!/^(\*|_)*awo+f?(!|\*|_)*( ?(:3|<3|owo|uwu))?( ?â¤ï¸)?(\*|_)*$/ui.test(msg.content)) {
+if (!/^(\*|_)*awo+f?(!|\*|_)*( ?(:3|<3|owo|uwu))?( ?❤️)?(\*|_)*$/ui.test(msg.content)) {
 msg.delete();
 msg.author.send("No " + msg.content + ", only awoo!");
 }
@@ -215,6 +367,4 @@ msg.author.send("No " + msg.content + ", only awoo!");
 
 
 });
-
-
 fc.login('YourTokenHere');
