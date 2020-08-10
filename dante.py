20ecf96e2f4f93c786a5ed7d79de7b4d8ed85cc8
Darkmane Arweinydd <mcpchalie@gmail.com>
2020-08-10 21:23:35 +0100

    Added some extra functions.
    Mainly checks fur invite links on club floof and deletes the words "dark is cute"

--- dante.py
+++ dante.py
@@ -3,6 +3,8 @@ import importlib
 
 from discord.ext import commands
 
+channels = [725776681159098408, 728691486115233804, 738967638964830341, 725566843766571112, 732288159060328529, 739199815627440232, 730887667491012720, 729753696199508088]
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
@@ -10,7 +12,21 @@ class MyClient(discord.Client):
     async def on_message(self, message):
 
         prefix = "!"
+        staff = False
         message.content = message.content.lower()
+        if 'dark' in message.content and 'cute' in message.content:
+            await message.delete()
+        if message.guild.id == 725201209358549012:
+            for role in message.author.roles:
+                if role.id  == 739727880799518741:
+                    staff = True
+            if 'discord.gg' in message.content:
+                if staff == True:
+                    return
+                if message.channel.id in channels:
+                    return
+                await message.channel.send("<@" + str(message.author.id) + "> Please do not promote your server here! \n\nIf you're looking to partner, please check <#729753696199508088>")
+                await message.delete()
         mentions = message.mentions
         if len(mentions) > 3:
             if not message.author.bot:
